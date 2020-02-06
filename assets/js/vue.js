4b6db477ed1ffd16e70c23d31af6f257d70e96ab
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2020-02-06 14:54:36 +0300

    initiat component

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 
-new Vue({
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li> this is my todo {{todo.text}}</li>'
+})
+
+var app  = new Vue({
     el: '#app',
     delimiters: ['${','}'],
     data: {
@@ -14,6 +19,17 @@ new Vue({
     methods: {
         addMessage: function () {
             this.messages.push({text: this.message})
+        }
+    },
+    created: function () {
+        console.log("show value of e: ");
+    },
+    mounted: function () {
+      console.log("is mounted thos");
+    },
+    computed: {
+        reverseMessage() {
+            return this.message.split('').reverse().join('');
         }
     }
 })
