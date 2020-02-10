d42b760b059ca349887641cfc0021c9c4ecc4045
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2020-02-10 15:33:53 +0300

    initiation component

--- assets/js/react.js
+++ assets/js/react.js
@@ -1,8 +1,18 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-
-ReactDOM.render(
-    <p>Hello world</p>,
+function Welcome(props) {
+    return <p>Hello {props.name}</p>;
+}
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>);
+}
+const element = <App />;
+ReactDOM.render(element,
     document.getElementById('root')
 )
