e04cd3907b6a2cb46d9599219fb37e039639cca4
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2020-02-10 15:45:26 +0300

    comment component

--- assets/js/react.js
+++ assets/js/react.js
@@ -1,18 +1,50 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-function Welcome(props) {
-    return <p>Hello {props.name}</p>;
-}
-function App() {
+function Comment(props) {
     return (
-        <div>
-            <Welcome name="Sara" />
-            <Welcome name="Cahal" />
-            <Welcome name="Edite" />
-        </div>);
+        <div className="Comment">
+            <div className="UserInfo">
+                <Avatar user={props.author} />
+                <div className="UserInfo-name">
+                    {props.author.name}
+                </div>
+            </div>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+            {formatDate(props.date)}
+            </div>
+        </div>
+    );
 }
-const element = <App />;
-ReactDOM.render(element,
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+    />
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}/>,
     document.getElementById('root')
 )
